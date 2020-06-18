dc525f39d569ce2e3e6f339b004febaf6768fde7
batman503daniel <54407239+batman503daniel@users.noreply.github.com>
2020-06-17 17:40:12 -0700

    Commit button

--- Source Code/Form1.cs
+++ Source Code/Form1.cs
@@ -18,7 +18,8 @@ namespace Arkanoid
 
         private void btn2_Click(object sender, EventArgs e)
         {
-            
+            Score score = new Score();
+            score.Show();
         }
 
         private void btn3_Click(object sender, EventArgs e)
